31d1793bf4676c2020647e2eebaa2da55c558b25
Jorge Humberto Ibarra Vazquez <jorgeh.ibarra.vazquez@gmail.com>
2024-12-05 18:38:51 -0600

    Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,100 +1,177 @@
 <!DOCTYPE html>
 <html lang="es">
+<!--begin::Head-->
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <meta name="description" content="PlataformaIngresos">
-    <meta name="keywords" content="PlataformaIngresos">
-    <meta name="author" content="PlataformaIngresos">
-    <meta name="robots" content="noindex, nofollow">
+    <base href="../../../" />
 
     <title>{{ config('app.name') }}</title>
-    <!-- Favicon -->
-    @include('layouts.partials.favicon')
 
-
-    <!-- ico-font-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/icofont.css') }}">
-    <!-- Themify icon-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/themify.css') }}">
-    <!-- Flag icon-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/flag-icon.css') }}">
-    <!-- Feather icon-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/feather-icon.css') }}">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/vendors/bootstrap.css') }}">
-
-    <!-- Main CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/color-1.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
+    <meta charset="utf-8" />
+    <meta name="description" content="The most advanced Tailwind CSS & Bootstrap 5 Admin Theme with 40 unique prebuilt layouts on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel versions. Grab your copy now and get life-time updates for free." />
+    <meta name="keywords" content="tailwind, tailwindcss, metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel starter kits, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Metronic - The World's #1 Selling Tailwind CSS & Bootstrap Admin Template by KeenThemes" />
+    <meta property="og:url" content="https://keenthemes.com/metronic" />
+    <meta property="og:site_name" content="Metronic by Keenthemes" />
+    <link rel="canonical" href="http://preview.keenthemes.comauthentication/layouts/overlay/sign-in.html" />
+    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+    <!--begin::Fonts(mandatory for all pages)-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
+    <!--end::Fonts-->
+    <!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
+    <link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+    <!--end::Global Stylesheets Bundle-->
+    <script>
+        // Frame-busting to prevent site from being loaded within a frame without permission (click-jacking) if (window.top != window.self) { window.top.location.replace(window.self.location.href); }
+    </script>
 </head>
+<!--end::Head-->
+<!--begin::Body-->
 
+<body id="kt_body" class="auth-bg bgi-size-cover bgi-attachment-fixed bgi-position-center">
+    <!--begin::Theme mode setup on page load-->
+    <script>
+        var defaultThemeMode = "light";
+        var themeMode;
+        if (document.documentElement) {
+            if (document.documentElement.hasAttribute("data-bs-theme-mode")) {
+                themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
+            } else {
+                if (localStorage.getItem("data-bs-theme") !== null) {
+                    themeMode = localStorage.getItem("data-bs-theme");
+                } else {
+                    themeMode = defaultThemeMode;
+                }
+            }
+            if (themeMode === "system") {
+                themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+            }
+            document.documentElement.setAttribute("data-bs-theme", themeMode);
+        }
+    </script>
+    <!--end::Theme mode setup on page load-->
+    <!--begin::Main-->
+    <!--begin::Root-->
+    <div class="d-flex flex-column flex-root">
+        <!--begin::Page bg image-->
+        <style>
+            body {
+                background-image: url('assets/media/auth/bg10.jpeg');
+            }
 
-<body class="light">
-    <!-- login page start-->
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xl-7 b-center bg-size">
-                <img class="bg-img-cover bg-center" src="../assets/images/login/2.jpg" alt="looginpage"
-                    style="display: none;">
-            </div>
-            <div class="col-xl-5 p-0">
-                <div class="login-card login-dark">
-                    <div>
-                        <div><a class="logo text-start" href="index.html"><img class="img-fluid for-light"
-                                    src="../assets/images/logo/logo.png" alt="looginpage"><img
-                                    class="img-fluid for-dark" src="../assets/images/logo/logo_dark.png"
-                                    alt="looginpage"></a></div>
-                        <div class="login-main">
-                            <form class="theme-form" method="post"  action="{{route('login.custom')}}">
-                              @csrf
-                                <h4>Acceso al sistema</h4>
-                                <p>Por favor captura tus credenciales</p>
-                                <p>
-                                  @include('layouts.alertas')
-                                  </p>
-                                <div class="form-group">
-                                    <label class="col-form-label">Email</label>
-                                    <input class="form-control" type="email" name="email" required=""
-                                        placeholder="Test@gmail.com">
-                                </div>
-                                <div class="form-group">
-                                    <label class="col-form-label">Password</label>
-                                    <div class="form-input position-relative">
-                                        <input class="form-control" type="password" name="password"
-                                            required="" placeholder="*********">
-                                    </div>
-                                </div>
-                                <div class="form-group mb-0">
-                                    <h4><a href="{{route('forgot-password')}}" class="hover-a">Olvidé mi password</a></h4>
-                                    <button class="btn btn-primary btn-block w-100 mt-4" type="submit">Entrar</button>
-                                </div>
-                            </form>
-                        </div>
+            [data-bs-theme="dark"] body {
+                background-image: url('assets/media/auth/bg10-dark.jpeg');
+            }
+        </style>
+        <!--end::Page bg image-->
+        <!--begin::Authentication - Sign-in -->
+        <div class="d-flex flex-column flex-lg-row flex-column-fluid">
+            <!--begin::Aside-->
+            <div class="d-flex flex-lg-row-fluid">
+                <!--begin::Content-->
+                <div class="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
+                    <!--begin::Image-->
+                    <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="assets/media/auth/agency.png" alt="" />
+                    <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="assets/media/auth/agency-dark.png" alt="" />
+                    <!--end::Image-->
+                    <!--begin::Title-->
+                    <h1 class="text-gray-800 fs-2qx fw-bold text-center mb-7">Fast, Efficient and Productive</h1>
+                    <!--end::Title-->
+                    <!--begin::Text-->
+                    <div class="text-gray-600 fs-base text-center fw-semibold">In this kind of post,
+                        <a href="#" class="opacity-75-hover text-primary me-1">the blogger</a>introduces a person they’ve interviewed
+                        <br />and provides some background information about
+                        <a href="#" class="opacity-75-hover text-primary me-1">the interviewee</a>and their
+                        <br />work following this is a transcript of the interview.
                     </div>
+                    <!--end::Text-->
                 </div>
+                <!--end::Content-->
             </div>
+            <!--begin::Aside-->
+            <!--begin::Body-->
+            <div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12">
+                <!--begin::Wrapper-->
+                <div class="bg-body d-flex flex-column flex-center rounded-4 w-md-600px p-10">
+                    <!--begin::Content-->
+                    <div class="d-flex flex-center flex-column align-items-stretch h-lg-100 w-md-400px">
+                        <!--begin::Wrapper-->
+                        <div class="d-flex flex-center flex-column flex-column-fluid pb-15 pb-lg-20">
+                            <!--begin::Form-->
+                            <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" method="post" action="{{route('login.custom')}}">
+                                @csrf
+                                <!--begin::Heading-->
+                                <div class="text-center mb-11">
+                                    <!--begin::Title-->
+                                    <h1 class="text-gray-900 fw-bolder mb-3">Acceso al sistema</h1>
+                                    <!--end::Title-->
+                                    <!--begin::Subtitle-->
+                                    <div class="text-gray-500 fw-semibold fs-6">Por favor captura tus credenciales</div>
+                                    <!--end::Subtitle=-->
+                                    <p>
+                                        @include('layouts.alertas')
+                                    </p>
+                                </div>
+
+
+                                <!--begin::Input group=-->
+                                <div class="fv-row mb-8">
+                                    <!--begin::Email-->
+                                    <input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent" />
+                                    <!--end::Email-->
+                                </div>
+                                <!--end::Input group=-->
+                                <div class="fv-row mb-3">
+                                    <!--begin::Password-->
+                                    <input type="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent" />
+                                    <!--end::Password-->
+                                </div>
+                                <!--end::Input group=-->
+                                <!--begin::Wrapper-->
+                                <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+                                    <div></div>
+                                    <!--begin::Link-->
+                                    <a href="{{route('forgot-password')}}" class="link-primary">Olvidé mi password</a>
+                                    <!--end::Link-->
+                                </div>
+                                <!--end::Wrapper-->
+                                <!--begin::Submit button-->
+                                <div class="d-grid mb-10">
+                                    <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
+                                        <!--begin::Indicator label-->
+                                        <span class="indicator-label">Entrar</span>
+                                        <!--end::Indicator label-->
+                                        <!--begin::Indicator progress-->
+                                        <span class="indicator-progress">Please wait...
+                                            <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                                        <!--end::Indicator progress-->
+                                    </button>
+                                </div>
+                                <!--end::Submit button-->
+                               
+                            </form>
+                            <!--end::Form-->
+                        </div>
+                        <!--end::Wrapper-->
+                    </div>
+                    <!--end::Content-->
+                </div>
+                <!--end::Wrapper-->
+            </div>
+            <!--end::Body-->
         </div>
-
-        <!-- jQuery -->
-        <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-
-
-        <!-- Bootstrap Core JS -->
-        <script src="{{ asset('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
-
-        <script src="{{ asset('assets/js/icons/feather-icon/feather.min.js') }}"></script>
-        <script src="{{ asset('assets/js/icons/feather-icon/feather.min.js') }}"></script>
-
-        <script src="{{ asset('assets/js/config.js') }}"></script>
-
-        <!-- Custom JS -->
-        <script src="{{ asset('assets/js/script.js') }}"></script>
-        <script src="{{ asset('assets/js/script1.js') }}"></script>
+        <!--end::Authentication - Sign-in-->
     </div>
+    <!--end::Root-->
+    <!--end::Main-->
+    <!--begin::Javascript-->
+    
+    <!--end::Javascript-->
 </body>
+<!--end::Body-->
 
 </html>
