9dd06fac27f987bbb3df6739804c3dbcb59cccc2
Juan Carlos Salinas Ojeda <juan.carlos@jasoluciones.com.mx>
2024-12-17 18:26:11 -0600

    Bienvenido

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,22 +1,46 @@
 @extends('layouts.mainlayout')
 
 @section('breadcrumb')
-<!--begin::Page title-->
 <div class="page-title d-flex flex-column align-items-start me-3 py-2 py-lg-0 gap-2">
-    <!--begin::Title-->
     <h1 class="d-flex text-gray-900 fw-bold m-0 fs-3">Home
-        <!--begin::Separator-->
         <span class="h-20px border-gray-500 border-start mx-3"></span>
-        <!--end::Separator-->
-        <!--begin::Description-->
-        <small class="text-gray-500 fs-7 fw-semibold my-1"></small>
-        <!--end::Description-->
+        <small class="text-gray-500 fs-7 fw-semibold my-1">Bienvenido</small>
     </h1>
-    <!--end::Title-->
 </div>
-<!--end::Page title-->
 @endsection
 @section('contenido')
+
+<div class="col-xl-12">
+
+<!--begin::Engage widget 1-->
+<div class="card h-md-100" dir="ltr">
+    <!--begin::Body-->
+    <div class="card-body d-flex flex-column flex-center">
+        <!--begin::Heading-->
+        <div class="mb-2">
+            <!--begin::Title-->
+            <!-- <h1 class="fw-semibold text-gray-800 text-center lh-lg">
+                Have your tried <br> new
+                <span class="fw-bolder"> Invoice Manager?</span>
+            </h1> -->
+            <!--end::Title-->
+
+            <!--begin::Illustration-->
+            <div class="py-10 text-center">
+                    <img src="{{ asset('assets/media/misc/asistencias.png') }}" class="theme-light-show w-400px" alt="">
+                    <img src="{{ asset('assets/media/misc/asistencias.png') }}" class="theme-dark-show  w-400px"  alt="">
+            </div>
+            <!--end::Illustration-->
+        </div>
+        <!--end::Heading-->
+
+
+    </div>
+    <!--end::Body-->
+</div>
+<!--end::Engage widget 1-->
+
+     </div>
 
 @endsection
 @section('scripts')
